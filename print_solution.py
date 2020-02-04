233dc8aea48ebb4bcca8d3c7ed46043489c6e938
iakobian <iakobian@gmail.com>
2020-02-04 16:53:39 +0600

    Remove find from calculate() function

--- print_solution.py
+++ print_solution.py
@@ -1,6 +1,6 @@
 from math import exp
-import functions
+import functions as fn
 
-solution = functions.get_solution()
+solution = fn.get_solution()
 for result in solution:
 	print("z =",exp(result[0])," mu =",result[0], " free_energy =",result[1], " density =",result[2])
