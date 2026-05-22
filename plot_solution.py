8f3696e32dfd61710bafe87b1351c55d2f08e2fb
IAKOBYPC\iakob <iakobian@gmail.com>
2026-05-22 19:30:55 +1200

    Calculation for any chemical potential

--- plot_solution.py
+++ plot_solution.py
@@ -1,14 +1,19 @@
-from math import exp
 import matplotlib.pyplot as plt
 import functions as fn
 
 chemical_potential = []
 density = []
-for pair in reversed(fn.compressibility_to_chemical_potential):
-	result = fn.calculate(pair[1])
-	chemical_potential.append(result[0])
-	density.append(result[2])
-	print("z =",exp(result[0])," mu =",result[0], " free_energy =",result[1], " density =",result[2])
+for result in fn.get_solution():
+	chemical_potential.append(result.chemical_potential)
+	density.append(result.density)
+	print(
+		"z =", result.activity,
+		" mu =", result.chemical_potential,
+		" free_energy =", result.free_energy,
+		" density =", result.density,
+		" x =", result.parameter,
+		" phase =", result.phase,
+	)
 plt.plot(chemical_potential, density)
 plt.xlabel('Chemical potential')
 plt.ylabel('Density')
--- print_solution.py
+++ print_solution.py
@@ -1,6 +1,12 @@
-from math import exp
 import functions as fn
 
 solution = fn.get_solution()
 for result in solution:
-	print("z =",exp(result[0])," mu =",result[0], " free_energy =",result[1], " density =",result[2])
+	print(
+		"z =", result.activity,
+		" mu =", result.chemical_potential,
+		" free_energy =", result.free_energy,
+		" density =", result.density,
+		" x =", result.parameter,
+		" phase =", result.phase,
+	)
